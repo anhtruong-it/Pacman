94fbd5358efc53d500f1c75f38d614948f7be817
ChrisVTP <78598914+ChrisVTP@users.noreply.github.com>
2021-08-23 00:28:08 +1000

    Make button

--- app_class.py
+++ app_class.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import pygame_menu
 from player import *
 from setting import *
 
@@ -15,6 +16,8 @@ class App :
         self.cell_h = MAZE_HEIGHT //COLS
         self.cell_w = MAZE_WIDTH //ROWS
         self.player = Player(self,PLAYER_START_POSITION)
+        self.button = {}
+        self.menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_DARK)   
         self.load()
     def run(self):
         while self.running :
@@ -26,6 +29,10 @@ class App :
                 self.playing_event()
                 self.playing_update()
                 self.playing_draw()
+            elif self.state == 'configure': #not coded yet
+                self.start_event()
+                self.start_update()
+                self.start_draw()
             else:
                 self.running = False
             self.clock.tick(FPS)
@@ -51,26 +58,64 @@ class App :
             pygame.draw.line(self.background, GREY , (x*self.cell_w, 0),(x*self.cell_w, HEIGHT))
         for y in range(HEIGHT//self.cell_h):
             pygame.draw.line(self.background, GREY , (0, y * self.cell_h),(WIDTH, y*self.cell_h))
+
+    def in_button (self, button_pos):
+        mouse_pos = pygame.mouse.get_pos()
+        pos, w,h = button_pos[:3]
+        return  mouse_pos[0] > pos[0] and mouse_pos[1]> pos[1] and mouse_pos[0] < pos[0] + w and mouse_pos[1] < pos[1] + h
+
+    def draw_button(self, word, screen, pos, w,h ,default_colour, state):
+        hollow_colour = self.in_button((pos,w,h))
+        rec = pygame.Rect(pos[0], pos[1], w, h)
+        if hollow_colour:
+            default_colour = (default_colour[0]/3, default_colour[1]/3,default_colour[2]/3)
+        pygame.draw.rect(screen, default_colour,rec ,  50, 10)
+        self.draw_text(word, screen, [(pos[0]+w//2) , (pos[1]+h//2) ], BUTTON_WORD_SIZE, WHITE, BUTTON_WORD_FONT, True)
+        if word not in self.button:
+            self.button[word] = (pos, w,h, state)
+        # self.screen.fill(default_colour, rec)
+
 ##################### START FUNCTION ################################33
 
     def start_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                self.state = 'playing'
-                print("Space press")
+            if event.type == pygame.MOUSEBUTTONUP:
+                for key in self.button:
+                    if self.in_button(self.button[key]):
+                        self.state = self.button[key][-1] #the state in the button tuple
+                        break
 
     def start_update(self):
         pass
     def start_draw(self):
         self.screen.fill (BLACK)
-        icon = pygame.transform.scale(self.icon, (100,100))
-        self.screen.blit(icon,[WIDTH//2 -50, HEIGHT//2 -150])
+        icon = pygame.transform.scale(self.icon, (400,100))
+        self.screen.blit(icon,[WIDTH//2 -200, HEIGHT//2 -150])
 
-        self.draw_text("PUSH SPACE BAR",self.screen, [WIDTH//2, HEIGHT//2], START_TEXT_SIZE, (170,132,58) , START_FONT, center = True)
-        self.draw_text("1 Player Only",self.screen, [WIDTH//2, HEIGHT//2 + 50], START_TEXT_SIZE, (25, 73, 215) , START_FONT, center = True)
+        self.draw_text("2805ICT System and Software Design",self.screen, [WIDTH//2, HEIGHT//2], START_TEXT_SIZE,(25, 73, 215)   , START_FONT, center = True)
+        self.draw_text("3815ICT Software Engineering ",self.screen, [WIDTH//2, HEIGHT//2 + 50], START_TEXT_SIZE, (25, 73, 215) , START_FONT, center = True)
+        self.draw_text("2021-Trimester 2",self.screen, [WIDTH//2, HEIGHT//2 + 100], START_TEXT_SIZE, (170,132,58), START_FONT, center = True)
         self.draw_text("HIGHEST SCORE: {}".format(self.score),self.screen, [4, 0], START_TEXT_SIZE, (255,255,255) , START_FONT)
+
+        #Draw Play game button
+        self.draw_button("Play", self.screen, [10, HEIGHT//2+150], BUTTON_W, BUTTON_H ,RED, "playing")
+        self.draw_button("Quit", self.screen, [WIDTH- BUTTON_W -10, HEIGHT//2+150], BUTTON_W, BUTTON_H ,RED, "quit")
+        self.draw_button("Confingure", self.screen, [WIDTH//2 - BUTTON_W//2, HEIGHT//2+150], BUTTON_W, BUTTON_H ,RED, "configure")
+        
+
+
+
+
+        # draw student name:
+        student_height_pos = HEIGHT - 60
+        for i in STUDENTS:
+            self.draw_text(i[0], self.screen, [10,student_height_pos], STUDENT_SIZE, (255,255,255), STUDENT_FRONT)
+            self.draw_text(i[1], self.screen, [170,student_height_pos], STUDENT_SIZE, (255,255,255), STUDENT_FRONT)
+            self.draw_text(i[2], self.screen, [250,student_height_pos], STUDENT_SIZE, (255,255,255), STUDENT_FRONT)
+            student_height_pos += 20
+        
         pygame.display.update()
 
 ##################### Playing FUNCTION ################################33
--- setting.py
+++ setting.py
@@ -12,6 +12,8 @@ BLACK = (0,0,0)
 RED = (255, 0, 0)
 GREY = (110, 110, 104)
 WHITE = (255,255,255)
+YELLOW = (255,255,0)
+
 #font
 START_TEXT_SIZE = 26
 START_FONT = 'Bauhaus 93'
@@ -20,3 +22,20 @@ START_FONT = 'Bauhaus 93'
 START_ICON = "pacmanLogo.png"
 #player setting
 PLAYER_START_POSITION = vec(1,1)
+
+# The year and course code
+COURSE_YEAR = "2021"
+# Students:
+STUDENT_SIZE = 20
+STUDENT_FRONT = "Cooper Std"
+STUDENTS = [("PHUC THIEN VUONG", "S5193954","2805ICT"),
+("ANH TRUONG NGUYEN", "S5168384","2805ICT"),
+("HIEN DAT CHU", "S5223891","3815ICT")
+]
+
+#Button setting
+BUTTON_W = 150
+BUTTON_H = 80
+
+BUTTON_WORD_SIZE = 30
+BUTTON_WORD_FONT = "Bauhaus 93"
